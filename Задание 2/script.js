c2ee24f5feace48dda0d25f3a79d82ef9652dd30
Anna Svetlova <svetloovaanna@gmail.com>
2022-02-04 03:31:19 +0300

    add flaf /g

--- Задание 2/script.js
+++ Задание 2/script.js
@@ -3,10 +3,10 @@ let comment = document.getElementById('comment').value;
 let lowerString = comment.toLowerCase();
 
 if(lowerString.includes('viagra')){
-    let changeViagra = lowerString.replace('viagra', "***");
+    let changeViagra = lowerString.replace(/viagra/g, "***");
     console.log(changeViagra);
 } else if (lowerString.includes('xxx')) {
-    let changeXxx = lowerString.replace('xxx', "***");
+    let changeXxx = lowerString.replace(/xxx/g, "***");
     console.log(changeXxx);
 } else console.log(lowerString);
 }
