c3dcf37c8c973a63975ada6932730c6d65d2af48
Anna Svetlova <svetloovaanna@gmail.com>
2022-02-14 22:46:52 +0300

    changed for

--- Задание 1/script.js
+++ Задание 1/script.js
@@ -8,17 +8,21 @@ function checkName() {
 
     let arrOfName = userName1.split(" ");
 
-    for (let names of arrOfName) {
-        names = names.toLowerCase(); 
-        names = names[0].toUpperCase() + names.slice(1);
-        console.log(names);
+    // for (let names of arrOfName) {     моя первоначальная версия для своего же ревью)
+    //     names = names.toLowerCase(); 
+    //     names = names[0].toUpperCase() + names.slice(1);
+    //     console.log(names);
+    // }
+
+    for(let i = 0; i< arrOfName.length; i++){
+        arrOfName[i] = arrOfName[i][0].toUpperCase() + arrOfName[i].slice(1).toLowerCase();
     }
 
-    // console.log(arrOfName);
+    console.log(arrOfName);
 
-    // document.getElementById('surname').value = arrOfName[0];
-    // document.getElementById('name').value = arrOfName[1];
-    // document.getElementById('second-name').value = arrOfName[2];
+    document.getElementById('surname').value = arrOfName[0];
+    document.getElementById('name').value = arrOfName[1];
+    document.getElementById('second-name').value = arrOfName[2];
 }
 
 
