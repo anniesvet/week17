3ea24fe9f33c8cb946d05c16aeb2941c6f7411cd
Anna Svetlova <svetloovaanna@gmail.com>
2022-02-16 21:07:32 +0300

    added changes

--- Задание 2/script.js
+++ Задание 2/script.js
@@ -2,15 +2,14 @@ function checkSpam() {
 let comment = document.getElementById('comment').value;
 let lowerString = comment.toLowerCase();
 
-if(lowerString.includes('viagra')){
-    let changeViagra = lowerString.replace(/viagra/g, "***");
-    console.log(changeViagra);
-} else if (lowerString.includes('xxx')) {
-    let changeXxx = lowerString.replace(/xxx/g, "***");
-    console.log(changeXxx);
-} else console.log(lowerString);
+let srtringChanged = lowerString.replace(/xxx|viagra/g, "***");
+console.log(srtringChanged);
+
+document.getElementById('message').innerHTML = srtringChanged;
 }
-console.log(checkSpam());
+
+
+
 
 
 
