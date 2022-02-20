04213beb87f25ff9d9015deaee9a6283237dd8b2
Anna Svetlova <svetloovaanna@gmail.com>
2022-02-20 14:43:23 +0300

    add changes

--- Задание 2/script.js
+++ Задание 2/script.js
@@ -1,19 +1,32 @@
-function checkSpam() {
-let comment = document.getElementById('comment').value;
-let lowerString = comment.toLowerCase();
+// function checkSpam() {
+// let comment = document.getElementById('comment').value;
+// let lowerString = comment.toLowerCase();
 
-let srtringChanged = lowerString.replace(/xxx|viagra/g, "***");
-console.log(srtringChanged);
+// let srtringChanged = lowerString.replace(/xxx|viagra/g, "***");
+// console.log(srtringChanged);
 
-document.getElementById('message').innerHTML = srtringChanged;
+// document.getElementById('message').innerHTML = srtringChanged;
+// }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let name = localStorage.getItem("name");
+    if(name != null) {
+        document.getElementById("name").value = name;
+    }
+});
+
+function sendMessage (author, comment) {
+    document.getElementById("chat").innerHTML = author + ': ' + comment;
 }
 
+function checkMessage() {
+    let author = document.getElementById("author").value;
+    let comment = document.getElementById("comment").value;
 
+    if(localStorage.getItem('name') == null) {
+        localStorage.setItem('name', author);
+    }
 
-
-
-
-
-
-
+    sendMessage(author, comment);
+}
 
